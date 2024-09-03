d876fe9e767ddc583397c821ff78ca112ba0f6ad
Josias Padilha <josiasdnl@gmail.com>
2024-09-02 23:13:19 -0300

    fix: correct IMC calculation, added new functionalities and update page styling
    - Fixed the incorrect IMC-Z calculation.
    - Modified dial and needle graphics to improve visual representation
    - Updated page styling

--- exercices/imc.js
+++ exercices/imc.js
@@ -1,127 +1,236 @@
 'strict mode';
 
+/*
+O cálculo do IMC é baseado na fórmula:
+IMC = _______(peso em kg)_______
+      (altura em cm / 100) ** 2
+
+Para crianças e adolescentes entre 5 e 19 anos, a classificação do estado nutricional é baseada no escore Z do IMC. A fórmula usada para chegar a esse índice é baseada no método LMS e foi tirada de um documento de instruções da OMS. Segue o link:
+
+https://cdn.who.int/media/docs/default-source/child-growth/growth-reference-5-19-years/computation.pdf?sfvrsn=c2ff6a95_4
+*/
+/*
+O gráfico suporta IMC de adultos com resultado entre 13 e 45 - 13 é o valor mínimo (0deg) e 45 é o valor máximo (180deg). 
+- Para obter a posição da seta, o valor mínimo (13) é subtraído do valor do IMC;
+- Então, os 180 graus são divididos em 32 partes iguais (ratio). O ratio representa a área abrangida entre o valor máximo (45) e o valor mínimo (13);
+- Isso leva à fórmula: (imc - valMin) * 180 / (ratio);
+*/
+
+// O valor de `finalScore` varia entre -4 e 4, dividindo 180 graus em 8 partes iguais. Mas para a fórmula de `calcNeedlePosition` funcionar, o valor mínimo precisa ser positivo. Por isso, `finalZScore` e menor valor são acrescidos de 4.
+
 const form = document.querySelector('.calc-form');
-const height = document.querySelector('#height');
-const weight = document.querySelector('#weight');
-const calcResult = document.querySelector('#imc-result');
-const needleGauge = document.querySelector('#needle');
-const classification = document.querySelector('#classification');
+const heightEl = document.querySelector('#height');
+const weightEl = document.querySelector('#weight');
+const abdomenEl = document.querySelector('#abdomen');
+const ageEl = document.querySelector('#age');
+const biotypeEl = document.querySelector('#biotype');
+const IMC = document.querySelector('#imc-result');
+const classificationEl = document.querySelector('#classification');
+const dial = document.querySelector('.dial');
+const needle = document.querySelector('#needle');
 const reset = document.querySelector('button[type="reset"]');
 
-const imcZBoys = {
-  5: [12.1, 13.0, 14.1, 15.3, 16.6, 18.3, 20.2],
-  6: [12.1, 13.0, 14.1, 15.3, 16.8, 18.5, 20.7],
-  7: [12.3, 13.1, 14.2, 15.5, 17.0, 19.0, 21.6],
-  8: [12.4, 13.3, 14.4, 15.7, 17.4, 19.7, 22.8],
-  9: [12.6, 13.5, 14.6, 16.0, 17.9, 20.5, 24.3],
-  10: [12.8, 13.7, 14.9, 16.4, 18.5, 21.4, 26.1],
-  11: [13.1, 14.1, 15.3, 16.9, 19.2, 22.5, 28.0],
-  12: [13.4, 14.5, 15.8, 17.5, 19.9, 23.6, 30.0],
-  13: [13.8, 14.9, 16.4, 18.2, 20.8, 24.8, 31.7],
-  14: [14.3, 15.5, 17.0, 19.0, 21.8, 25.9, 33.1],
-  15: [14.7, 16.0, 17.6, 19.8, 22.7, 27.0, 34.1],
-  16: [15.1, 16.5, 18.2, 20.5, 23.5, 27.9, 34.8],
-  17: [15.3, 16.8, 18.6, 20.9, 24.0, 28.3, 35.0],
-  18: [15.7, 17.3, 19.2, 21.7, 24.9, 29.2, 35.4],
-  19: [15.9, 17.6, 19.6, 22.2, 25.4, 29.7, 35.5],
+// Tabela LMS para meninos de 5 a 19 anos
+const lmsBoys = {
+  5: [-0.7387, 15.2641, 0.0839],
+  6: [-0.9921, 15.3062, 0.08682],
+  7: [-1.246, 15.4832, 0.09068],
+  8: [-1.4629, 15.7368, 0.09526],
+  9: [-1.6318, 16.049, 0.10038],
+  10: [-1.7407, 16.4433, 0.10566],
+  11: [-1.7862, 16.9392, 0.1107],
+  12: [-1.7751, 17.5334, 0.11522],
+  13: [-1.7168, 18.233, 0.11898],
+  14: [-1.6211, 19.005, 0.12191],
+  15: [-1.4961, 19.7744, 0.12412],
+  16: [-1.3529, 20.4951, 0.12579],
+  17: [-1.1962, 21.1423, 0.12715],
+  18: [-1.026, 21.7077, 0.12836],
+  19: [-0.8419, 22.1883, 0.12948],
 };
-const imcZGirls = {
-  5: [11.8, 12.7, 13.9, 15.2, 16.9, 18.9, 21.3],
-  6: [11.7, 12.7, 13.9, 15.3, 17.0, 19.2, 22.1],
-  7: [11.8, 12.7, 13.9, 15.4, 17.3, 19.8, 23.3],
-  8: [11.9, 12.9, 14.1, 15.7, 17.7, 20.6, 24.8],
-  9: [12.1, 13.1, 14.4, 16.1, 18.3, 21.5, 26.5],
-  10: [12.4, 13.5, 14.8, 16.6, 19.0, 22.6, 28.4],
-  11: [12.7, 13.9, 15.3, 17.2, 19.9, 23.7, 30.2],
-  12: [13.2, 14.4, 16.0, 18.0, 20.8, 25.0, 31.9],
-  13: [13.6, 14.9, 16.6, 18.8, 21.8, 26.2, 33.4],
-  14: [14.0, 15.4, 17.2, 19.6, 22.7, 27.3, 34.7],
-  15: [14.4, 15.9, 17.8, 20.2, 23.5, 28.2, 35.5],
-  16: [14.6, 16.2, 18.2, 20.7, 24.1, 28.9, 36.1],
-  17: [14.7, 16.4, 18.4, 21.0, 24.5, 29.3, 36.3],
-  18: [14.7, 16.4, 18.6, 21.3, 24.8, 29.5, 36.3],
-  19: [14.7, 16.5, 18.7, 21.4, 25.0, 29.7, 36.2],
+
+// Tabela LMS para meninas de 5 a 19 anos
+const lmsGirls = {
+  5: [-0.8886, 15.2441, 0.09692],
+  6: [-1.0794, 15.2697, 0.10195],
+  7: [-1.2565, 15.4036, 0.10746],
+  8: [-1.388, 15.681, 0.11291],
+  9: [-1.465, 16.0964, 0.11816],
+  10: [-1.4864, 16.6133, 0.12307],
+  11: [-1.4606, 17.2459, 0.12748],
+  12: [-1.4006, 17.9966, 0.13129],
+  13: [-1.3195, 18.8012, 0.13445],
+  14: [-1.2266, 19.5647, 0.137],
+  15: [-1.1311, 20.2125, 0.13904],
+  16: [-1.0368, 20.7008, 0.1407],
+  17: [-0.9423, 21.0367, 0.14208],
+  18: [-0.8462, 21.2603, 0.1433],
+  19: [-0.7496, 21.4269, 0.14441],
 };
 
 // Reset
 reset.addEventListener('click', () => {
-  needleGauge.style.transform = 'translate(-100%, 0) rotate(0deg)';
-  classification.textContent = '';
-  classification.style.backgroundColor = '';
+  needle.style.transform = 'translate(-100%, 0) rotate(0deg)';
+  classificationEl.textContent = '';
+  classificationEl.style.backgroundColor = '';
 });
 
-// Função para calcular IMC
-const calc = (height, weight) => {
-  const h = parseFloat(height.value / 100);
-  console.log(h);
-  const w = parseFloat(weight.value.replace(',', '.'));
-  return (w / (h * h)).toFixed(2);
-};
+// Calcula a direção da seta no gráfico
+function calcNeedlePosition(imc, valMin, ratio) {
+  const needlePosition = ((imc - valMin) * 180) / ratio;
 
-// Representação gráfica do resultado
-
-/*
-O diagrama suporta IMC com resultados entre 13 e 45. Então, 13 é o valor mínimo (0deg) e 45 é o valor máximo (180deg). 
-- Para obter um índice, subtraia o valor mínimo (13) do valor do IMC;
-- Esse valor deve ser representado num gráfico de 0 a 180 graus. Para isso, divida 180 por 32, que é a diferença entre o valor máximo (45) e o valor mínimo (13);
-- Chegamos a fórmula: (imc - 13) * 180 / (45 - 13)
-- Ou, simplificando: (imc - 13) * 5.625
-*/
-
-function diagram(imc) {
-  return (imc - 13) * 5.625;
-}
-
-// Exibir classificação de acordo com resultado do IMC
-function imcCategory(imc) {
-  if (imc < 17) {
-    classification.textContent = 'Muito abaixo do peso';
-    classification.style.backgroundColor = '#f4a261';
-  } else if (imc < 18.5) {
-    classification.textContent = 'Abaixo do peso';
-    classification.style.backgroundColor = '#e9c46a';
-  } else if (imc < 25) {
-    classification.textContent = 'Peso desejável';
-    classification.style.color = '#2a9d8f';
-  } else if (imc < 30) {
-    classification.textContent = 'Sobrepeso';
-    classification.style.backgroundColor = '#e9c46a';
-  } else if (imc < 35) {
-    classification.textContent = 'Obesidade grau I';
-    classification.style.backgroundColor = '#f4a261';
-  } else if (imc < 40) {
-    classification.textContent = 'Obesidade grau II (severa)';
-    classification.style.backgroundColor = '#e76f51';
+  if (needlePosition > 180) {
+    return 180;
+  } else if (needlePosition < 0) {
+    return 0;
   } else {
-    classification.textContent = 'Obesidade grau III (mórbida)';
-    classification.style.backgroundColor = '#bc4749';
+    return needlePosition;
   }
 }
 
-function displayCalcResult(result, style, needle, category, background) {
-  calcResult.textContent = result;
-  calcResult.style.fontStyle = style;
-  needleGauge.style.transform = needle;
+// Exibir classificação de acordo com resultado do IMC
+function displayIMC(imc, isAdult, imcValue, needlePosition) {
+  // Mapeamento das faixas de IMC e suas respectivas classificações e cores
+  const classAdult = [
+    { max: 17, text: 'Muito abaixo do peso', color: 'var(--orange)' },
+    { max: 18.5, text: 'Abaixo do peso', color: 'var(--yellow)' },
+    { max: 25, text: 'Peso desejável', color: 'var(--green)' },
+    { max: 30, text: 'Sobrepeso', color: 'var(--yellow)' },
+    { max: 35, text: 'Obesidade grau I', color: 'var(--orange)' },
+    { max: 40, text: 'Obesidade grau II (severa)', color: 'var(--light-red)' },
+    {
+      max: Infinity,
+      text: 'Obesidade grau III (mórbida)',
+      color: 'var(--dark-red)',
+    },
+  ];
+
+  const classChild = [
+    { max: -3, text: 'Muito abaixo do peso', color: 'var(--light-red)' },
+    { max: -2, text: 'Abaixo do peso', color: 'var(--yellow)' },
+    { max: 1, text: 'Peso desejável', color: 'var(--green)' },
+    { max: 2, text: 'Sobrepeso', color: 'var(--yellow)' },
+    { max: Infinity, text: 'Obesidade', color: 'var(--light-red)' },
+  ];
+
+  let classification;
+
+  if (isAdult) {
+    classification = classAdult.find(c => imc < c.max);
+  } else {
+    classification = classChild.find(c => imc < c.max);
+  }
+
+  // Atualiza as informações do gráfico
+  if (classification) {
+    classificationEl.textContent = classification.text;
+    classificationEl.style.backgroundColor = classification.color;
+    IMC.textContent = imcValue;
+    needle.style.transform = needlePosition;
+  }
 }
 
-// Detecta quando botão submit é pressionado
+// Validação dos dados inseridos no formulário
+function validateNumbers(value, element, minValue) {
+  if (value < minValue || isNaN(value)) {
+    element.style.border = '1.5px solid var(--error-color)';
+    return false;
+  } else {
+    element.style.border = '';
+    return true;
+  }
+}
+
+// Detecta quando o botão submit é pressionado no formulário
 form.addEventListener('submit', e => {
   e.preventDefault();
 
-  const calcResult = calc(height, weight);
-  imcCategory(calcResult);
+  const height = parseInt(heightEl.value);
+  const weight = parseFloat(weightEl.value.replace(',', '.'));
+  const abdomen = parseFloat(abdomenEl.value.replace(',', '.'));
+  const age = parseInt(ageEl.value);
+  const maleOrFemale = biotypeEl.checked;
 
-  if (isNaN(calcResult)) {
-    displayCalcResult(
-      `Dados incorretos`,
-      'italic',
-      'translate(-100%, 0) rotate(0deg)'
-    );
-  } else {
-    displayCalcResult(
-      `IMC igual a ${calcResult.replace('.', ',')}`,
-      '',
-      `translate(-100%, 0) rotate(${diagram(calcResult)}deg)`
-    );
+  let isValid = true;
+
+  // Chama a função de validação dos dados
+  isValid = validateNumbers(height, heightEl, 1);
+  isValid = validateNumbers(weight, weightEl, 1);
+  isValid = validateNumbers(abdomen, abdomenEl, 1);
+  isValid = validateNumbers(age, ageEl, 5);
+
+  if (isValid) {
+    // Cálculo do IMC
+    const calcIMC = (weight / (height / 100) ** 2).toFixed(2);
+    console.log('IMC: ', calcIMC);
+
+    if (age > 19) {
+      // Gira o gráfico para exibir o mostrador para adultos
+      dial.style.transform = 'rotate(-90deg)';
+
+      // Chama função que converte o resultado em um ângulo entre 0 e 180 graus
+      const needlePosition = calcNeedlePosition(calcIMC, 13, 45 - 13);
+
+      displayIMC(
+        calcIMC,
+        true,
+        `Seu IMC é: ${calcIMC.replace('.', ',')}`,
+        `translate(-100%, 0) rotate(${needlePosition}deg)`
+      );
+    } else {
+      // Gira o gráfico para exibir o mostrador para crianças e adolescentes
+      dial.style.transform = 'rotate(90deg)';
+
+      // Cálculo do escore Z para 19 anos ou menos
+      function finalScoreAndBiotype(lms) {
+        const [l, m, s] = lms[age];
+
+        // Escore Z inicial
+        const zScore = ((calcIMC / m) ** l - 1) / (s * l);
+
+        // Ajuste do desvio padrão em valores < -3 ou > 3
+        function zScoreAdjustment(x, y) {
+          const sd3 = m * (1 + l * s * x) ** (1 / l);
+          console.log('sd3: ', sd3);
+          const sd2 = m * (1 + l * s * y) ** (1 / l);
+          console.log('sd2: ', sd2);
+          if (x > 0) {
+            const sd23 = sd3 - sd2;
+            return (x + (calcIMC - sd3)) / sd23;
+          } else {
+            const sd23 = sd2 - sd3;
+            return (x + (calcIMC - sd3)) / sd23;
+          }
+        }
+
+        // Escore Z final
+        const finalZScore = () => {
+          if (zScore > 3) {
+            return zScoreAdjustment(3, 2);
+          } else if (zScore < -3) {
+            return zScoreAdjustment(-3, -2);
+          } else {
+            return zScore;
+          }
+        };
+
+        // Chama função que converte o resultado em um ângulo entre 0 e 180 graus
+        const needlePosition = calcNeedlePosition(finalZScore() + 4, 0, 8);
+
+        displayIMC(
+          finalZScore(),
+          false,
+          `Seu escore Z é: ${finalZScore().toFixed(2).replace('.', ',')}`,
+          `translate(-100%, 0) rotate(${needlePosition}deg)`
+        );
+      }
+
+      // Identifica o sexo da criança para extrair os dados corretos
+      if (maleOrFemale) {
+        finalScoreAndBiotype(lmsGirls);
+      } else {
+        finalScoreAndBiotype(lmsBoys);
+      }
+    }
   }
 });
